1d64f7201519009dbe7413cd46f5b1ba11fe3a7a
mario5735522 <mario@petmediagroup.com>
2026-04-01 09:47:03 +0200

    26. Remove Duplicates from Sorted Array.py

---
+++ Python/26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates_brute_force(self, nums: List[int]) -> int:
+        unique = 0
+        visited_map = {}
+        for i in range(len(nums)):
+            if nums[i] in visited_map:
+                nums[i] = 101
+            else:
+                visited_map[nums[i]] = True
+                unique += 1
+        nums.sort()
+        return unique
+        
+    def removeDuplicates(self, nums: List[int]) -> int:
+        current = 101
+        unique = 0
+        for i in range(len(nums)):
+            if nums[i] == current:
+                nums[i] = 101
+            else:
+                current = nums[i]
+                unique += 1
+
+        nums.sort()
+        return unique
+
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.removeDuplicates([1, 1, 2]))
