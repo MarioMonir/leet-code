dd6f1e415fc673a1bc8ebd7bb5e1f7142a381ab6
mario5735522 <mario3monir@gmail.com>
2026-04-02 13:29:29 +0200

    Refactor rotate method in 189. Rotate Array.py to use in-place reversal for improved efficiency

--- Python/189. Rotate Array.py
+++ Python/189. Rotate Array.py
@@ -1,15 +1,24 @@
 from typing import List
 
 class Solution:
+    def reverse_in_place(self, nums:List[int], startIndex: int, endIndex: int)->None:
+        if endIndex >= len(nums):
+            return
+
+        while startIndex < endIndex:
+            nums[startIndex],nums[endIndex] = nums[endIndex],nums[startIndex]
+            startIndex+=1
+            endIndex-=1
+
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            last = nums[-1]
-            for j in range(len(nums)-1, 0, -1):
-                nums[j] = nums[j-1]
-            nums[0] = last
+        k = k % len(nums)
+        self.reverse_in_place(nums,0,len(nums)-1)
+        self.reverse_in_place(nums,0,k-1)
+        self.reverse_in_place(nums,k,len(nums)-1)
+
 
 
         
@@ -19,11 +28,17 @@ if __name__ == "__main__":
     nums = [1,2,3,4,5,6,7]
     k = 3
     solution.rotate(nums, k)
-    # [5, 6, 7, 1, 2, 3, 4]
     print(nums)
+    # [5, 6, 7, 1, 2, 3, 4]
 
     nums = [-1,-100,3,99]
     k = 2
     solution.rotate(nums, k)
-    # [3, 99, -1, -100]
     print(nums)
+    # [3, 99, -1, -100]
+
+
+    nums=[-1]
+    solution.rotate(nums, 2)
+    print(nums)
+    # [-1]
