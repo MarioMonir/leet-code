9c265475d12005ab85f9bce76936c01ce02b8a52
mario5735522 <mario3monir@gmail.com>
2026-04-07 12:02:38 +0200

    Refactor implementation of LeetCode problem 12 - Integer to Roman, replacing dictionary with a list of tuples for symbol mapping and adding example usage for testing.

--- Python/12. Integer to Roman.py
+++ Python/12. Integer to Roman.py
@@ -1,11 +1,35 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-          int_to_roman = {
-            1: 'I',
-            5: 'V',
-            10: 'X',
-            50: 'L',
-            100: 'C',
-            500: 'D',
-            1000: 'M'
-        }
+        symbols = [
+            ('I',1),
+            ('IV', 4),
+            ('V', 5),
+            ('IX', 9),
+            ('X', 10),
+            ('XL', 40),
+            ('L', 50),
+            ('XC', 90),
+            ('C', 100),
+            ('CD', 400),
+            ('D', 500),
+            ('CM', 900),
+            ('M', 1000),
+        ]
+
+        decimal=""
+
+        for roman_symbol , decimal_value in reversed(symbols):
+            if num // decimal_value:
+                count = num // decimal_value
+                decimal += (roman_symbol * count)
+                num = num % decimal_value
+
+        return decimal
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.intToRoman(3) == "III")
+    print(solution.intToRoman(4) == "IV")
+    print(solution.intToRoman(9) == "IX")
+    print(solution.intToRoman(58) == "LVIII")
+    print(solution.intToRoman(1994) == "MCMXCIV")
